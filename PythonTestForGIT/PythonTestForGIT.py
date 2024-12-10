6d59841e9ed20f2203be27f51bdc31d8265b813a
Marcello <marcello.avoni@gmail.com>
2024-12-10 17:45:22 +0100

    Trying to use git

--- PythonTestForGIT/PythonTestForGIT.py
+++ PythonTestForGIT/PythonTestForGIT.py
@@ -1,1 +1,11 @@
+## Here wi try to learn use python
+# Simple Python Script to Test
 
+# Print a welcome message
+print("Hello, Marcello! This is a simple Python script.")
+
+# Calculate the sum of numbers from 1 to 10
+sum_numbers = sum(range(1, 11))
+
+# Print the result
+print("The sum of numbers from 1 to 10 is:", sum_numbers)
